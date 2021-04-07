c99abfdcd85d739661b972d1066d9f9a23b15c32
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2021-04-07 23:26:28 +0430

    changing theme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,8 +2,30 @@ import {merge} from "theme-ui"
 import theme from '@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui/index'
 import "@fontsource/architects-daughter"
 import "@fontsource/changa"
+import { tailwind } from "@theme-ui/presets"
 
 export default {
     ...theme,
     initialColorModeName: `dark`,
+    colors: {
+        primary: tailwind.colors.purple[5],
+        secondary: `#5f6c80`,
+        toggleIcon: tailwind.colors.gray[8],
+        heading: tailwind.colors.black,
+        divide: tailwind.colors.gray[4],
+        background: `#efefef`,
+        muted: tailwind.colors.gray[4],
+        modes: {
+            light: {
+                text: tailwind.colors.gray[4],
+                primary: tailwind.colors.purple[5],
+                secondary: `#7f8ea3`,
+                toggleIcon: tailwind.colors.gray[4],
+                background: `#1A202C`,
+                heading: tailwind.colors.white,
+                divide: tailwind.colors.gray[8],
+                muted: tailwind.colors.gray[8],
+            },
+        },
+    },
 }
